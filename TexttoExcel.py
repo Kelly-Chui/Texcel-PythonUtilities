be5dbe15e0cfa4f3311f13d86bd4f6247ea350b7
Kelly-Chui <optioneldar@gmail.com>
2022-10-28 05:08:34 +0900

    [T2E] add Appinfo, show in Explorer, adjust UI

--- TexttoExcel.py
+++ TexttoExcel.py
@@ -7,6 +7,7 @@
 
 import os
 import re
+import webbrowser
 from openpyxl import Workbook
 import tkinter as tk
 from tkinter import filedialog
@@ -16,12 +17,12 @@ def selectLoadFolder():
     global loadPath
     loadPath = filedialog.askdirectory(initialdir = "/", title = "Open text data")
     if loadPath == "":
-        tk.messagebox.showwarning("Warning!", "It's not a folder.")
+        tk.messagebox.showwarning("Warning", "It's not a folder.")
     else:
         res = os.listdir(loadPath)
         print(res)
         if len(res) == 0:
-            messagebox.showwarning("Warning!", "There are no files inside the folder.")
+            messagebox.showwarning("Warning", "There are no files inside the folder.")
         else:
             loadPathLabel.config(text=loadPath)
             for file in res:
@@ -31,7 +32,11 @@ def selectSaveFolder():
     global savePath
     savePath = filedialog.askdirectory(initialdir = "/", title = "Select a save directory")
     savePathLabel.config(text=savePath)
+
 def makeExcelFile(savePath):
+    if loadPath == "" or savePath == "":
+        messagebox.showwarning("Warning", "Directories must be selected")
+        return
     files = os.listdir(loadPath)
     files = list(filter(lambda x: ".txt" in x, files))
     files.sort(key = lambda x: int(''.join(re.findall(r'\d+', x))))
@@ -48,19 +53,42 @@ def makeExcelFile(savePath):
         row += 1
     resultFile.save(savePath + "/" + "result" + ".xlsx")
     print("end")
+    messagebox.showinfo("Complete", "Complete")
+
+def openExplorer():
+    os.system(f"explorer {os.path.realpath(savePath)}")
+
+def openURL():
+    webbrowser.open("https://github.com/Kelly-Chui/PythonUtilities")
+
+def opensourceURL():
+    webbrowser.open("https://openpyxl.readthedocs.io/en/stable/")
+
+def showAppinfo():
+    appInfoView = tk.Toplevel()
+    appInfoView.title("App infomation")
+    appInfoView.geometry("200x200")
+    appInfoView.resizable(False, False)
+    maker = tk.Label(appInfoView, text = "Made by Kelly Chui")
+    githubURL = tk.Button(appInfoView, text = "contact(Website)", command = openURL)
+    openSourceLabel = tk.Label(appInfoView, text = "Opensource License")
+    openSourceInfo = tk.Button(appInfoView, text = "openpyxl", command = opensourceURL)
+
+    maker.pack(side="top")
+    githubURL.pack(side="top")
+
+    openSourceInfo.pack(side="bottom")
+    openSourceLabel.pack(side="bottom")
 
 resultFile = Workbook()
 resultFile.remove(resultFile["Sheet"])
 sheet = resultFile.create_sheet(index = 1, title = "result")
 
 rootView = tk.Tk()
-rootView.title = "TextToExcel"
-
+rootView.configure(bg = "white")
+rootView.title("TextToExcel")
 rootView.geometry("600x400")
 rootView.resizable(False, False)
-
-loadPathLabel = tk.Label(rootView, text = "")
-savePathLabel = tk.Label(rootView, text = "")
 
 file = tk.Frame(rootView)
 file.pack(fill = "x", padx = 5, pady = 5)
@@ -69,14 +97,29 @@ loadPath = ""
 savePath = ""
 textList = []
 
-loadButton = tk.Button(file, text = "Load", width = 12, padx = 5, pady = 5, command = selectLoadFolder)
-saveButton = tk.Button(file, text = "Save", width = 12, padx = 5, pady = 5, command = selectSaveFolder)
-proceedButton = tk.Button(rootView, text = "Run", width = 12, padx = 15, pady = 15, command = lambda: makeExcelFile(savePath))
+loadLabel = tk.Label(rootView, bg = "white", height = 10)
+loadLabel.pack(side = "top", pady = 10)
+saveLabel = tk.Label(rootView, bg = "white", height = 10)
+saveLabel.pack(side = "top", pady = 10)
+runLabel = tk.Label(rootView, bg = "white")
+runLabel.pack()
+infoLabel = tk.Label(rootView, bg = "white", height = 20)
+infoLabel.pack(side = "bottom")
 
-loadButton.pack(padx = 5, pady = 5)
-loadPathLabel.pack()
-saveButton.pack(padx = 10, pady = 5)
-savePathLabel.pack()
-proceedButton.pack()
+loadPathLabel = tk.Label(loadLabel, text = "loadPath", width = "30")
+savePathLabel = tk.Label(saveLabel, text = "savePath", width = "30")
+loadButton = tk.Button(loadLabel, text = "Load", bg = "white", width = 5, command = selectLoadFolder)
+saveButton = tk.Button(saveLabel, text = "Save", width = 5, bg = "white", command = selectSaveFolder)
+proceedButton = tk.Button(runLabel, text = "Run", fg = "white", bg = "green", width = 10, pady = 5, command = lambda: makeExcelFile(savePath))
+openSaveButton = tk.Button(runLabel, text = "Show results in Explorer", bg = "white", fg = "blue", width = 20, command = openExplorer)
+infoButton = tk.Button(infoLabel, text = "App info", bg = "white", fg = "green", command = showAppinfo)
+
+loadButton.pack(side = "right", padx = 10)
+loadPathLabel.pack(side = "right")
+saveButton.pack(side = "right", padx = 10)
+savePathLabel.pack(side = "right")
+proceedButton.pack(side = "top")
+openSaveButton.pack(side = "top")
+infoButton.pack()
 
 rootView.mainloop()
